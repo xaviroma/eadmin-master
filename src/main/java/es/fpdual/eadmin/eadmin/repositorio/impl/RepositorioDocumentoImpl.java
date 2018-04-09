81b8b5938db401c7e7df6f12300177948e4edcb5
xaviroma <xaviroma7@gmail.com>
2018-04-09 13:05:08 +0200

    5

--- src/main/java/es/fpdual/eadmin/eadmin/repositorio/impl/RepositorioDocumentoImpl.java
+++ src/main/java/es/fpdual/eadmin/eadmin/repositorio/impl/RepositorioDocumentoImpl.java
@@ -58,8 +58,9 @@ public class RepositorioDocumentoImpl implements RepositorioDocumento {
 		}
 
 		documentos.add(documento);
+		logger.info("Documento agregado.");
 		listarEnAlta(documento);
-		exportExcel("AltaDocumentos",documento, "documentos_excel.xlsx");
+		exportExcel("AltaDocumentos", documento, "documentos_excel.xlsx");
 
 		logger.info(documento.toString() + " creado correctamente.");
 
@@ -282,32 +283,31 @@ public class RepositorioDocumentoImpl implements RepositorioDocumento {
 	}
 
 	public static boolean exportExcel(String nombreHoja, Documento documento, String fileName) {
-		
+
 		Map<String, Object[]> documentos = new TreeMap<String, Object[]>();
 		Integer numeroLineas = 0;
-		
+
 		File archivoExcel = new File(fileName);
 		if (archivoExcel.exists()) {
 			// Cabecera
-			documentos.put("0", new Object[] {"ID", "NOMBRE", "FECHA DE CREACION", "PUBLICO"});
+			documentos.put("0", new Object[] { "ID", "NOMBRE", "FECHA DE CREACION", "PUBLICO" });
 			numeroLineas++;
-			
+
 		} else {
 			ArrayList<String[]> datosExcel = importExcel(fileName, 4);
 			ListIterator<String[]> it = datosExcel.listIterator();
-			
-			while(it.hasNext()) {
+
+			while (it.hasNext()) {
 				numeroLineas++;
 				String[] datos = it.next();
 				documentos.put(numeroLineas.toString(), datos);
-				
+
 			}
 		}
-		
+
 		numeroLineas++;
-		documentos.put(numeroLineas.toString(),new Object[] {documento.getCodigo(), documento.getNombre(),
+		documentos.put(numeroLineas.toString(), new Object[] { documento.getCodigo(), documento.getNombre(),
 				documento.getFechaCreacion().toString(), documento.getPublico().toString() });
-		
 
 		// Creamos el libro de trabajo
 		XSSFWorkbook libro = new XSSFWorkbook();
@@ -349,70 +349,70 @@ public class RepositorioDocumentoImpl implements RepositorioDocumento {
 			return false;
 		}
 	}
-	
+
 	public static ArrayList<String[]> importExcel(String fileName, int numColums) {
 
 		// ArrayList donde guardaremos todos los datos del excel
-			ArrayList<String[]> data = new ArrayList<>();
+		ArrayList<String[]> data = new ArrayList<>();
 
-			try {
-				// Acceso al fichero xlsx
-		FileInputStream file = new FileInputStream(new File(fileName));
+		try {
+			// Acceso al fichero xlsx
+			FileInputStream file = new FileInputStream(new File(fileName));
 
 			// Creamos la referencia al libro del directorio dado
-				XSSFWorkbook workbook = new XSSFWorkbook(file);
+			XSSFWorkbook workbook = new XSSFWorkbook(file);
 
-				// Obtenemos la primera hoja
-				XSSFSheet sheet = workbook.getSheetAt(0);
+			// Obtenemos la primera hoja
+			XSSFSheet sheet = workbook.getSheetAt(0);
 
-				// Iterador de filas
-				Iterator<Row> rowIterator = sheet.iterator();
+			// Iterador de filas
+			Iterator<Row> rowIterator = sheet.iterator();
 
-				while (rowIterator.hasNext()) {
-					Row row = rowIterator.next();
-					// Iterador de celdas
-			Iterator<Cell> cellIterator = row.cellIterator();
-	// contador para el array donde guardamos los datos de cada fila
-					int contador = 0;
-				// Array para guardar los datos de cada fila 
-					// y añadirlo al ArrayList
-					String[] fila = new String[numColums];
-					// iteramos las celdas de la fila
-			while (cellIterator.hasNext()) {
+			while (rowIterator.hasNext()) {
+				Row row = rowIterator.next();
+				// Iterador de celdas
+				Iterator<Cell> cellIterator = row.cellIterator();
+				// contador para el array donde guardamos los datos de cada fila
+				int contador = 0;
+				// Array para guardar los datos de cada fila
+				// y añadirlo al ArrayList
+				String[] fila = new String[numColums];
+				// iteramos las celdas de la fila
+				while (cellIterator.hasNext()) {
 					Cell cell = cellIterator.next();
 
-			// Guardamos los datos de la celda segun su tipo
-						switch (cell.getCellType()) {
-						//si es numerico
+					// Guardamos los datos de la celda segun su tipo
+					switch (cell.getCellType()) {
+					// si es numerico
 					case Cell.CELL_TYPE_NUMERIC:
-			fila[contador] = (int) cell.getNumericCellValue() + "";
-							break;
-						// si es cadena de texto
+						fila[contador] = (int) cell.getNumericCellValue() + "";
+						break;
+					// si es cadena de texto
 					case Cell.CELL_TYPE_STRING:
-			fila[contador] = cell.getStringCellValue() + "";
-							break;
-						}
-			// Si hemos terminado con la ultima celda de la fila
-					if ((contador + 1) % numColums == 0) {
-			// Añadimos la fila al ArrayList con todos los datos
-							data.add(fila);
-						}
-		// Incrementamos el contador
-		// con cada fila terminada al redeclarar arriba el contador,
-			// no obtenemos excepciones de ArrayIndexOfBounds
-						contador++;
+						fila[contador] = cell.getStringCellValue() + "";
+						break;
 					}
+					// Si hemos terminado con la ultima celda de la fila
+					if ((contador + 1) % numColums == 0) {
+						// Añadimos la fila al ArrayList con todos los datos
+						data.add(fila);
+					}
+					// Incrementamos el contador
+					// con cada fila terminada al redeclarar arriba el contador,
+					// no obtenemos excepciones de ArrayIndexOfBounds
+					contador++;
 				}
-				// Cerramos el fichero y workbook
-				file.close();
-				workbook.close();
-			} catch (Exception e) {
-				e.printStackTrace();
 			}
-
-			System.out.println("Excel importado correctamente\n");
-
-			return data;
+			// Cerramos el fichero y workbook
+			file.close();
+			workbook.close();
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
+
+		System.out.println("Excel importado correctamente\n");
+
+		return data;
+	}
 
 }
